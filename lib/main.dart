6bd110bacec101235f507ea329e54217cc5a3b3a
mtarikg <mtarikgol@gmail.com>
2022-04-05 04:40:51 +0300

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tirbuschon_feng497/welcomePage.dart';
 import 'package:tirbuschon_feng497/Review/reviewVenuePage.dart';
 import 'package:tirbuschon_feng497/Review/reviewPersonnel.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
